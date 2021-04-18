170a986379735ee6f694bcd5f3889e63849d9cc2
CiraCas <zyra_cas@hotmail.com>
2021-04-18 20:32:17 +0200

    tabla gatos en adopcion y estilos en backoffice

--- application/models/m_gatos.php
+++ application/models/m_gatos.php
@@ -5,11 +5,14 @@ class m_gatos extends CI_Model {
     function select_gatos(){
         $this->db->select("*");
         $this->db->from("gatos");
-        
+        $this->db->join("adopciones", "gatos.idgato=adopciones.idgato2", "left outer");
+        $this->db->where("adopciones.idgato2", null);
         $query=$this->db->get();
-        
-        return $query->result_array();
-        
+        if($query->num_rows()>0){
+            return $query->result_array();
+        }else{
+            return NULL;
+        }
     }
 }
 ?>
--- application/views/v_admin_gatos.php
+++ application/views/v_admin_gatos.php
@@ -1,20 +1,38 @@
 <h1>Gatete</h1>
-<table id="table_id" class="display" border='1'>
+<table id="table_id" class="display tabla-admin" border='1'>
     <thead>
         <tr>
             <th>id</th>
             <th>Nombre</th>
             <th>Sexo</th>
+            <th>Descripción</th>
+            <th></th>
 
         </tr>
     </thead>   
     <tbody>
-        <?php var_dump($gatos);foreach( $gatos as $gato ): ?>
-            <tr>
-                <td><?=$gato['idgato']?></td>
-                <td><?=$gato['nombre']?></td>
-                <td><?=$gato['sexo']?></td>
-            </tr>
-        <?php endforeach; ?>
+        <?php 
+        if($gatos != NULL) {
+            
+            foreach( $gatos as $gato ): 
+                var_dump ($gato);?>
+            var_dump ($gato);
+                <tr>
+                    <td><?=$gato['idgato']?></td>
+                    <td><?=$gato['nombre']?></td>
+                    <td><?=$gato['sexo']?></td>
+                    <td><?=$gato['descripcion']?></td>
+                    <td class= "casilla-iconos">
+                        <i class="fas fa-trash-alt icon-admin"></i>
+                        <i class="fas fa-edit"></i>
+                    </td>
+
+                </tr>
+            <?php endforeach; 
+        }else{
+            echo "No hay gatos en adopción en estos momentos";
+        }
+        ?>
+
     </tbody>
 </table> 
